96e82f74030ae4e936476f3027b59a3c323b5126
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-06-25 22:00:42 -0800

    Exercise 2.8: File Extensions
    In this program I learned how to implement the function .removesuffix() which is exactly similar to .removesuffix() in the way that the remove redundant texts by declaring the unwanted text within a string inside the parentheses.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap2/file_extensions_2.8.py
@@ -0,0 +1,8 @@
+#Python has a removesuffix() method that works
+#exactly like removeprefix(). Assign the value 'python_notes.txt' to a variable
+#called filename. Then use the removesuffix() method to display the filename
+#without the file extension, like some file browsers do.
+
+filename = 'python_notes.txt'
+short_filename = f"{filename.removesuffix('.txt')}"
+print(short_filename)

d1de089ac588420494777cb76055620494edb31c
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-06-25 21:47:02 -0800

    Exercise 2.5: Famous Quote
    More practice

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap2/famous_quote_2.5.py
@@ -0,0 +1,5 @@
+quote ='"Throw off the bowlines, sail away from the safe harbor, catch the trade winds in your sails. Explore, Dream, Discover,"'
+author = 'mark twain'
+compiled = f"{author.title()} once said,{quote}"
+
+print(compiled)

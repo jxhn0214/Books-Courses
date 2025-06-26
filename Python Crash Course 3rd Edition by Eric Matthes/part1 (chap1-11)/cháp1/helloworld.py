77bd656710d1f347d03d84bbbfc22cb5bae62dc0
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-06-25 20:14:35 -0800

    Chap1 Helloworld
    In this program I learned that the print function, "print()" outputs, to the terminal, whatever is inside the parenthesis, most commonly a string. I also learned how to run a program, you use cd to navigate to the directory in which your program is located and then you do python3 filename to run the program. In the next chapter I will be learning about data types.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/cháp1/helloworld.py
@@ -0,0 +1,3 @@
+# This is my first program of the book, I have little experience in coding, I only know the basics
+
+print("Hello, world!") #Print function outputs the string inside the parentheses

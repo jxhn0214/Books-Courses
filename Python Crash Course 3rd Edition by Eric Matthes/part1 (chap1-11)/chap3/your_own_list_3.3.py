b14b10abf067524735d7593974f4559f50a7ffe1
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-06-27 10:28:53 -0800

    Exercise 3.3: Your Own List
    More practice.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap3/your_own_list_3.3.py
@@ -0,0 +1,18 @@
+"""
+
+Think of your favorite mode of transportation, such as
+a motorcycle or a car, and make a list that stores several examples. Use
+your list to print a series of statements about these items, such as I would
+like to own a Honda motorcycle.
+
+"""
+
+def main():
+    realistic = ['GR86', 'Supra MK5']
+    unrealistic = ['LFA', 'ZL1', 'Laferrari']
+
+    print(f"some of my realistic dream cars are: {realistic[0]} and {realistic[1]}")
+    print(f"some of my unrealistic dream cars are: {unrealistic[1]} and {unrealistic[-1]}")
+
+
+main()

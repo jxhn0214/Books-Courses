0324dca66380a1ebe18c9242dea444047297735f
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-06-27 10:24:20 -0800

    Exercise 3.1: Name
    In this program i learned to use the list function in python to create a list of names. I also knew, from CS50P,  that to create a function you can use the def statement so I used that to create my main function. I also learned that you can use sting manipulating method .title() on elements of a list if they are of string type.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap3/name_3.1.py
@@ -0,0 +1,14 @@
+'''
+Store the names of a few of your friends in a list called names.
+Print each person’s name by accessing each element in the list, one at a
+time.
+'''
+
+def main():
+    names = ['derek', 'tristan', 'aiden', 'jeimie']
+    print(names[0].title())
+    print(names[1].title())
+    print(names[-2].title())
+    print(names[-1].title())
+
+main()

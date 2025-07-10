a26e6656313cff5ce68865cd08e717e891b48296
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-09 19:38:28 -0800

    Exercise 4.1: Pizzas
    In this lesson I learned how to use a for loop in a list. I learned that indentation is vital to get a program to print out exactly how you want it because if I were to have indented the last print call it would have printed "Pizza is one of my favorite foods" for every single iteration of element on the list. I also learned that you should have good naming conventions for for loops like for "dog in dogs" or "for cat in cats" so it will be easier to read and keep track of in the future.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap4/pizzas_4.1.py
@@ -0,0 +1,7 @@
+def main():
+    pizzas = ['pepperoni', 'meat lovers', 'hot mama']
+    for pizza in pizzas:
+        print(f"I like {pizza} pizza.")
+    print("Pizza is one of my favorite foods!")
+
+main()

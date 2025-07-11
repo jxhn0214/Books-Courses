a5b5ffe9f639ed226be5db604ba456fdbd13d377
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-11 11:13:48 -0800

    Exercise 5.1: Conditional Tests
    In this program I am practicing the conditionals that I learned in this chapter thus far. I implemented the comparison operator "==" to check whether or not the variable value was equal to the guess and then printed out boolean values accordingly.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap5/conditional_tests_5.1.py
@@ -0,0 +1,46 @@
+def main():
+    pizza_topping = "pepperoni"
+
+    print("'pizza_topping == pepperoni?' I predict True")
+    print(pizza_topping == "pepperoni")
+
+    print("\n'pizza_topping == pineapple?' I predict False")
+    print(pizza_topping == "pineapple")
+
+
+    car = "ferrari"
+
+    print("\n'car == ferrari?' I predict True")
+    print(car == "ferrari")
+
+    print("\n'car == porsche?' I predict False")
+    print(car == "porsche")
+
+
+    pet = "cat"
+
+    print("\n'pet == cat?' I predict False")
+    print(pet == "cat")
+
+    print("\n'pet == dog?' I predict True")
+    print(pet == "dog")
+
+
+    wild_animal = "bear"
+
+    print("\n'wild_animal == tiger?' I predict True")
+    print(wild_animal == "bear")
+
+    print("\n'wild_animal == bear' I predict False")
+    print(wild_animal == "tiger")
+
+
+    country = "china"
+
+    print("\n'country == USA' I predict True")
+    print(country == "USA")
+
+    print("\n'country == china' I predict False")
+    print(country == "china")
+
+main()

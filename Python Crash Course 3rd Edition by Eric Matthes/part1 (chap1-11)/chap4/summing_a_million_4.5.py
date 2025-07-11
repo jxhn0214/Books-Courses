4df01cd039f11ed6d92f9b34577e3d17071fc36a
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-10 18:24:10 -0800

    Exercise 4.5: Summing A Million
    In this program I practiced how to use the functions min() and max() to test if my list was really from 1 to 1 million and then i used sum() to sum up all the values in the list, thus testing python's computing power.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap4/summing_a_million_4.5.py
@@ -0,0 +1,8 @@
+def main():
+    million = [value for value in range (1, 1000001)]
+    print(f"The minimum value in this list is: {min(million)}")
+    print(f"The maximum value in this list is: {max(million)}\n")
+
+    print(f"The sum of all the numbers in this list is: {sum(million)}")
+
+main()

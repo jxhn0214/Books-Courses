3db793e09f4470ac51b9df9de2601d8dad72fd51
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-11 18:49:51 -0800

    Exercise 5.6: Stage of Life
    More if-elif-else block practice.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap5/stages_of_life_5.6.py
@@ -0,0 +1,17 @@
+def main():
+    age = 17
+
+    if age < 2:
+        print("you are a baby")
+    elif age < 4:
+        print("you are a todler")
+    elif age < 13:
+        print("you are a kid")
+    elif age < 20:
+        print("you are a teen")
+    elif age < 65:
+        print("you are an adult")
+    else: 
+        print("you are an elder")
+
+main()

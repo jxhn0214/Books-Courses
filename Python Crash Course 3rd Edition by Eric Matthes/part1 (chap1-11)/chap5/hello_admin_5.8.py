ad2a80c139f5f097eefdee087198b70f66c7acfc
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-11 19:35:02 -0800

    Exercise 5.8: Hello Admin
    More practice.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap5/hello_admin_5.8.py
@@ -0,0 +1,12 @@
+def main():
+    usernames = ['bushcamper67', 'outdoorboy8', 'admin', 'happycamper78']
+
+    for username in usernames:
+        if username == 'admin':
+            print(f"hello {username}, would you like to see today's report.")
+        else:
+            print(f"hello {username}, thank you or signing back in again.")
+
+main()
+        
+            

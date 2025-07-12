b0dc1e00129bcfe03ffe7ef077d5c866f2b98b05
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-11 18:41:42 -0800

    Exercise 5.5: Alien Colors 3
    In this program I am practicing how to use if-elif-else blocks.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap5/alien_colors_3_5.5.py
@@ -0,0 +1,36 @@
+def main():
+    #green status
+    print("Alien green status: ")
+    alien_color = "green"
+
+    if alien_color == "green":
+        print("you just gained 5 points\n")
+    elif alien_color == "yellow":
+        print("you just earned 10 points\n")
+    else:
+        print("you just gained 15 points\n")
+    
+
+    print("Alien yellow status: ")
+    alien_color = "yellow"
+
+    if alien_color == "green":
+        print("you just gained 5 points\n")
+    elif alien_color == "yellow":
+        print("you just earned 10 points\n")
+    else:
+        print("you just gained 15 points\n")
+    
+
+    print("Alien red status: ")
+    alien_color = "red"
+
+    if alien_color == "green":
+        print("you just gained 5 points\n")
+    elif alien_color == "yellow":
+        print("you just earned 10 points\n")
+    else:
+        print("you just gained 15 points\n")
+
+
+main()

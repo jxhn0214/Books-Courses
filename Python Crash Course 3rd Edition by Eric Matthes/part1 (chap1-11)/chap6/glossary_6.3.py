0e5023a7bc1c1cd3f88a4bc15426e9903b7f34a9
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-13 12:29:29 -0800

    Exercise 6.3: Glossary
    More dictionary practice

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap6/glossary_6.3.py
@@ -0,0 +1,16 @@
+def main():
+    glossary = {
+        'list': 'a list of elements', 
+        'tuple': 'a non-dynamic list',
+        'dictionary': 'a list that holds key-values', 
+        'conditional': 'a statement that returns True or False', 
+        'for loop': 'a loop composed of intializer, conditon, iterator'
+    }
+
+    print(f"A list is {glossary['list']}")
+    print(f"A tuple is {glossary['tuple']}")
+    print(f"A dictionary is {glossary['dictionary']}")
+    print(f"a conditional is {glossary['conditional']}")
+    print(f"a for loop is {glossary['for loop']}")
+
+main()

8ae4b2990cd2b3db2e226c7edac709f4237489c9
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-13 12:21:00 -0800

    Exercise 6.2: Favorite Numbers
    More dictionary practice.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap6/favorite_numbers_6.2.py
@@ -0,0 +1,16 @@
+def main():
+    favorite_numbers = {
+        'john': 7,
+        'teddy': 18,
+        'george': 93,
+        'franklin': 44,
+        'william': 83, 
+    }
+
+    print(f"John's favorite number is {favorite_numbers['john']}")
+    print(f"Teddy's favorite number is {favorite_numbers['teddy']}")
+    print(f"George's favorite number is {favorite_numbers['george']}")
+    print(f"Franklin's favorite number is {favorite_numbers['franklin']}")
+    print(f"William's favorite number is {favorite_numbers['william']}")
+
+main()

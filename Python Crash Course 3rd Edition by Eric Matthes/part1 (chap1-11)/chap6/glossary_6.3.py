c2db6e7987ee1f946596ef93ac7024c59e4242e8
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-14 12:48:49 -0800

    Exercise 6.4: Glossary 2
    New and improved Glossary with a for loop instead of repetitive print calls. I learned many new methods since the last exercise like .values(), .keys(), .items(). As well as the function set() which removes duplicates by creating a set of unique items.

--- Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap6/glossary_6.3.py
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap6/glossary_6.3.py
@@ -4,13 +4,16 @@ def main():
         'tuple': 'a non-dynamic list',
         'dictionary': 'a list that holds key-values', 
         'conditional': 'a statement that returns True or False', 
-        'for loop': 'a loop composed of intializer, conditon, iterator'
+        'for loop': 'a loop composed of intializer, conditon, iterator',
+        '.keys()': 'method loops through the keys in a specified dictionary',
+        '.values()': 'method loops through the keys in a specified dictionary',
+        'set()': 'is a function that creates a set of unique items (without duplicates)',
+        'set': 'is essentially a dictionary without key-value pairs',
+        '.items()': 'is a method that assigns our keys and values to two indiviudal variables'
     }
 
-    print(f"A list is {glossary['list']}")
-    print(f"A tuple is {glossary['tuple']}")
-    print(f"A dictionary is {glossary['dictionary']}")
-    print(f"a conditional is {glossary['conditional']}")
-    print(f"a for loop is {glossary['for loop']}")
+    for word, definition in glossary.items(): 
+        print(f"A {word} is {definition}")
+
 
 main()

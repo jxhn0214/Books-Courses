33c6739ed3219a13eb1f457b65f9505f7ddec622
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-17 11:17:20 -0800

    Exercise 8.9: Messages
    passing lists as arguments practice.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap8/messages_8.9.py
@@ -0,0 +1,6 @@
+def show_messages(messages):
+    for message in messages: 
+        print (message)
+
+messages = ['hi', 'hello', 'im good', 'what time is it?', "what's up?"]
+show_messages(messages)

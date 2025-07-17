27045f30db5768a75cc6737330c4687f0a9eb80d
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-17 11:28:40 -0800

    Exercise 8.10: Sending Messages
    More practice with passing lists as arguments.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap8/sending_messages_8.10.py
@@ -0,0 +1,19 @@
+def show_messages(messages):
+    print("Pending Messages: ", end="")
+
+    for message in messages: 
+        print(message, end=", ")
+
+def send_messages(messages, sent_messages):
+    while messages:
+        pending_message = messages.pop()
+        sent_messages.append(pending_message)
+    return sent_messages
+messages = ['hi', 'hello', 'im good', 'what time is it?', "what's up?"]
+sent_messages = []
+
+sent = send_messages(messages, sent_messages)
+
+print(f"Messages sent: {sent}")
+print(f"Messages pending: {messages}")
+

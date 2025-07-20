d9419849edb6ecd40ab9f9fd22a97055d7fa79d9
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-20 15:54:25 -0800

    exercise-9.14: lottery
    In this program I was told to create a random list or tuple of 10 numbers and 5 letters. Then I researched a function from the random module that would choose 4 random elements from the list of random_chars and it was the function called sample.

---
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap9/lottery_9.14.py
@@ -0,0 +1,9 @@
+from random import sample
+
+random_chars = (1, 42, 23, 5, 61, 23, 6, 14, 76, 56, 'j', 'h', 'i', 'a', 'w')
+winning_chars = print("Any ticket with these matching letters/numbers wins a" 
+                      f"prize: {sample(random_chars, 4)}")
+
+
+
+

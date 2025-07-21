c91841a41f74aeee8f91650568bb7bff2d9facb5
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-07-21 10:52:23 -0800

    exercise-11.2: population
    previous exercise with a population parameter added as well as a new Unit Test for ones with populations.

--- Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap11/city_country_module.py
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap11/city_country_module.py
@@ -1,3 +1,6 @@
-def structure_city_country(city, country):
-    formatted_city_country = f"{city}, {country}"
+def structure_city_country(city, country, population=''):
+    if population:
+        formatted_city_country = f"{city}, {country} - population {population}"
+    else:
+        formatted_city_country = f"{city}, {country}"
     return formatted_city_country.title()
--- Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap11/test_city_country.py
+++ Python Crash Course 3rd Edition by Eric Matthes/part1 (chap1-11)/chap11/test_city_country.py
@@ -5,3 +5,9 @@ def test_city_country():
 
     formatted_address = structure_city_country('san francisco', 'united states of america')
     assert formatted_address == "San Francisco, United States Of America"
+
+def test_city_country_pop():
+    """do addresses like Tokyo, Japan - population size work?"""
+
+    formatted_address = structure_city_country('tokyo', 'japan', '37,000,000')
+    assert formatted_address == "Tokyo, Japan - Population 37,000,000"
